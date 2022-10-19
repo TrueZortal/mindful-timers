bb8b118af830065941642203cafe5d3f7d020310
Mateusz Buczek <mtsbck@gmail.com>
2022-10-19 21:03:21 +0200

    moving to postgres for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_19_142635) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "timers", force: :cascade do |t|
     t.integer "initial_value"
     t.datetime "created_at", null: false
